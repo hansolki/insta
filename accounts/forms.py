7dac9dcf650ce5792f6b22c029462c4f1fa1e60e
parkhansol <145949904+hansolki@users.noreply.github.com>
2023-11-09 13:46:16 +0900

    account signup 기능구현

---
+++ accounts/forms.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.forms import UserCreationForm
+from . models import User
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('username', 'profile_image', )
